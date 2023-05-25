34cf52ce7d03471c12644ac19a931dd8dd9b9d3f
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-25 10:28:28 +0100

    comit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Main } from './pages/main/main'
 import { Login } from './pages/login'
 import { Navbar } from './components/Navbar'
 import { CreatePost } from './pages/create-post/create-post'
-import  {Any}
+import { Anytest } from './pages/learn/anytest'
 function App() {
   return (
     <div className="App">
