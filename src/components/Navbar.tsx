3ce298826fb2c633b38c79e75af226a69b4748a0
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-25 10:56:22 +0100

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,6 +12,7 @@ export const Navbar = () => {
     <div className="navbar">
       <div className="links">
         <Link to="/">Home</Link>
+        <Link to="/anytestpage">Test</Link>
         {!user ? (
           <Link to="/login">Login</Link>
         ) : (
