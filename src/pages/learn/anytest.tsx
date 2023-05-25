c367ce1080da9bd795facb7219f679f7a9ffaf56
hamzatalha77 <34461042+hamzatalha77@users.noreply.github.com>
2023-05-25 11:41:31 +0100

    commit

--- src/pages/learn/anytest.tsx
+++ src/pages/learn/anytest.tsx
@@ -1,55 +1,7 @@
-import { useReducer } from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react'
 
-const initialTodos = [
-  {
-    id: 1,
-    title: 'Todo 1',
-    complete: false,
-  },
-  {
-    id: 2,
-    title: 'Todo 2',
-    complete: false,
-  },
-]
-
-const reducer = (state: any, action: any) => {
-  switch (action.type) {
-    case 'COMPLETE':
-      return state.map((todo: any) => {
-        if (todo.id === action.id) {
-          return { ...todo, complete: !todo.complete }
-        } else {
-          return todo
-        }
-      })
-    default:
-      return state
-  }
+const AnyTest = () => {
+  return <div>anytest</div>
 }
 
-export const AnyTest = () => {
-  const [todos, dispatch] = useReducer(reducer, initialTodos)
-
-  const handleComplete = (todo: any) => {
-    dispatch({ type: 'COMPLETE', id: todo.id })
-  }
-
-  return (
-    <>
-      {todos.map((todo: any) => (
-        <div key={todo.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={todo.complete}
-              onChange={() => handleComplete(todo)}
-            />
-            {todo.title}
-          </label>
-        </div>
-      ))}
-    </>
-  )
-}
+export default AnyTest
